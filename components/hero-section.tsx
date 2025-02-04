9324c6dc08cfb3e97aea59c1060461586212b44b
samitkapoor <samitkapoor77@gmail.com>
2025-02-04 22:56:02 +0530

    added entry animation to hero elements

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Lottie from 'lottie-react';
+import { motion } from 'framer-motion';
 
 import DotBackground from './ui/dot-background';
 import heroAnimation1 from '../public/hero-animation1.json';
@@ -16,9 +17,19 @@ const HeroSection = () => {
         <DotBackground className="justify-center gap-20 h-full p-20">
           <div className="inline-flex flex-col items-start justify-start relative">
             <div className="relative inline-flex items-start justify-start">
-              <p className="text-[150px] leading-none absolute -top-10 opacity-40 -left-10 z-0">
+              <motion.p
+                initial={{
+                  opacity: 0,
+                  y: '-400px'
+                }}
+                animate={{
+                  opacity: 0.4,
+                  y: '-40px'
+                }}
+                className="text-[150px] leading-none absolute opacity-40 -left-10 z-0"
+              >
                 💥
-              </p>
+              </motion.p>
               <p className="text-[55px] font-medium leading-none z-30">
                 Accelerate Your
                 <br />
@@ -28,18 +39,38 @@ const HeroSection = () => {
                   className="font-extrabold text-yellow-500 text-[60px] leading-tight"
                   encryptedClassName="font-semibold text-neutral-100 text-[60px] leading-tight"
                   sequential={true}
+                  speed={30}
                   text="Lightning Speed."
                   animateOn="view"
                 />
               </p>
-              <p className="text-[250px] leading-none absolute -bottom-10 opacity-40 -right-20 z-0">
+              <motion.p
+                initial={{
+                  opacity: 0,
+                  y: '-800px'
+                }}
+                animate={{
+                  opacity: 0.4,
+                  y: '10px',
+                  transition: { ease: ['backInOut'], duration: 1 }
+                }}
+                className="text-[250px] leading-none absolute opacity-40 -right-20 z-0"
+              >
                 🚀
-              </p>
+              </motion.p>
             </div>
-            <p className="text-[20px] leading-none mt-10 max-w-[700px]">
+            <motion.p
+              initial={{ opacity: 0, y: '200px', fontWeight: 'bold' }}
+              animate={{
+                opacity: 1,
+                y: '0px',
+                fontWeight: 'normal'
+              }}
+              className="text-[20px] leading-none mt-10 max-w-[700px]"
+            >
               A growing library of reusable snippets to help you ship projects faster, empowering
               developers with time-saving tools and ready-to-use code for seamless integration.
-            </p>
+            </motion.p>
           </div>
           <div className="flex flex-col gap-[20px]">
             <div className="flex items-center gap-[20px]">
