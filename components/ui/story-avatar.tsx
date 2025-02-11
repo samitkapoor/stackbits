a6b07736440a8b4e622330edfbb2c0835a962731
samitkapoor <samitkapoor77@gmail.com>
2025-02-11 15:41:52 +0530

    added comments to story avatar

--- components/ui/story-avatar.tsx
+++ components/ui/story-avatar.tsx
@@ -2,30 +2,35 @@ import React from 'react';
 
 const StoryAvatar = ({
   children,
-  spin = false,
-  flicker = false,
+  spin = false, // * true value would make the background gradient spin
+  flicker = false, // * true value would add a neon glowing shadow flicker effect
   backgroundClassName,
   wrapperClassName,
   className
 }: {
+  // * Remove these data types for javascript
   children?: React.ReactNode;
   spin?: boolean;
   flicker?: boolean;
   backgroundClassName?: string;
   wrapperClassName?: string;
   className?: string;
+  // * ----
 }) => {
   return (
+    // * wrapper div
     <div
       className={`rounded-full overflow-hidden p-[5px] flex items-center justify-center relative ${wrapperClassName} ${
         flicker && 'flicker'
       }`}
     >
+      {/* background gradient div */}
       <div
         className={`rounded-full ${
           spin && 'spin'
         } overflow-hidden bg-gradient-to-br from-orange-400 to-pink-800 h-full w-full absolute z-0 ${backgroundClassName}`}
       />
+      {/* your component wrapper div */}
       <div className={`rounded-full overflow-hidden border-[1px] border-black z-10 ${className}`}>
         {children}
       </div>
--- data/frontend/StoryAvatar.tsx
+++ data/frontend/StoryAvatar.tsx
@@ -85,28 +85,33 @@ export const storyAvatar: Document = {
         code: `import React from 'react';
         
 const StoryAvatar = ({
-    children,
-    spin = false,
-    flicker = false,
-    backgroundClassName,
-    wrapperClassName,
-    className
+  children,
+  spin = false, // * true value would make the background gradient spin
+  flicker = false, // * true value would add a neon glowing shadow flicker effect
+  backgroundClassName,
+  wrapperClassName,
+  className
 }: {
-    children?: React.ReactNode;
-    spin?: boolean;
-    flicker?: boolean;
-    backgroundClassName?: string;
-    wrapperClassName?: string;
-    className?: string;
+  // * Remove these data types for javascript
+  children?: React.ReactNode;
+  spin?: boolean;
+  flicker?: boolean;
+  backgroundClassName?: string;
+  wrapperClassName?: string;
+  className?: string;
+  // * ----
 }) => {
-    return (
-        <div className={\`rounded-full overflow-hidden p-[5px] flex items-center justify-center relative \${wrapperClassName} \${flicker && 'flicker'}\`}>
-            <div className={\`rounded-full \${spin && 'spin'} overflow-hidden bg-gradient-to-br from-orange-400 to-pink-800 h-full w-full absolute z-0 \${backgroundClassName}\`}/>
-            <div className={\`rounded-full overflow-hidden border-[1px] border-black z-10 \${className}\`}>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    // * wrapper div
+    <div className={\`rounded-full overflow-hidden p-[5px] flex items-center justify-center relative \${wrapperClassName} \${flicker && 'flicker'}\`} >
+      {/* background gradient div */}
+      <div className={\`rounded-full \${spin && 'spin'} overflow-hidden bg-gradient-to-br from-orange-400 to-pink-800 h-full w-full absolute z-0 \${backgroundClassName}\`} />
+      {/* your component wrapper div */}
+      <div className={\`rounded-full overflow-hidden border-[1px] border-black z-10 \${className}\`}>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default StoryAvatar;
