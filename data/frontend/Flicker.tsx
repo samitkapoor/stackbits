7a113bd8caf4d22cdcc5a1e6e0422cc625cb3d23
samitkapoor <samitkapoor77@gmail.com>
2025-02-12 17:21:56 +0530

    improved flicker presentation

--- data/frontend/Flicker.tsx
+++ data/frontend/Flicker.tsx
@@ -55,13 +55,17 @@ export const flicker: Document = {
         code: `import React from 'react';
         
 const FlickerBox = ({ children }: { children?: React.ReactNode }) => {
-  return <div className="flicker px-10 py-10 rounded-xl bg-[#1A1A2E]">
-    {children} 
-  </div>;
+  return (
+    <div
+      style={{ background: 'radial-gradient(circle at left, #1a1a2e, #2f2f2f)'}}
+      className="flicker w-[250px] h-[130px] sm:w-[300px] sm:h-[180px] text-white flex justify-center items-center text-[20px] font-bold rounded-[12px] text-center relative duration-200 hover:scale-105"
+    >
+      {children}
+    </div>
+  );
 };
 
-export default FlickerBox;
-        `
+export default FlickerBox;`
       },
       {
         heading: 'Usage',
