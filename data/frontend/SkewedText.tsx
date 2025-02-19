1ce7756735b08caf812fef25998b54d10f87300b
samitkapoor <samitkapoor77@gmail.com>
2025-02-19 12:19:39 +0530

    updated skewedtext animation

--- data/frontend/SkewedText.tsx
+++ data/frontend/SkewedText.tsx
@@ -53,23 +53,16 @@ const SkewedText = ({ children, className }: { children: React.ReactNode; classN
 
     const rect = pRef.current.getBoundingClientRect();
 
-    // ? Making sure the values of y remain between 30 and -30
-    const posY: number = Math.max(e.clientX - rect.left, 0);
-    let y = Math.min(posY / rect.width, 1) * 60;
-    if (y < 30) y = +(30 - y);
-    else if (y >= 30) y = -(y - 30);
+    const mx = e.clientX - rect.left;
+    const my = e.clientY - rect.top;
 
-    // ? Making sure the values of x remain between 10 and -10
-    const posX: number = Math.max(e.clientY - rect.top, 0);
-    let x = Math.min(posX / rect.height, 1) * 20;
-    if (x < 10) x = +(10 - x);
-    else if (x >= 10) x = -(x - 10);
+    const width = rect.right - rect.left;
+    const height = rect.bottom - rect.top;
 
-    if (posY >= 0) {
-      pRef.current.style.transform = \`perspective(300px) rotateX(\${x + 10}deg) rotateY(\${
-        y + 3
-      }deg)\`;
-    }
+    const xd = (mx - width / 2) / 8;
+    const yd = (height / 2 - my) / 8;
+
+    pRef.current.style.transform = \`perspective(1000px) rotateY(\${xd + 15}deg) rotateX(\${yd}deg)\`;
   };
 
   return (
@@ -97,7 +90,7 @@ const SkewedText = ({ children, className }: { children: React.ReactNode; classN
           opacity: 1,
           transition: { ease: 'backOut', duration: 1.2 }
         }}
-        className={\`font-bold opacity-0 text-center w-[75%] italic \${className}\`}
+        className={\`font-bold transition-all duration-75 opacity-0 text-center w-[75%] italic \${className}\`}
       >
         {children}
       </motion.p>
