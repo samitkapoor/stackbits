ba6b5e79865dafcf1297a59bcc4f561c53a8f47d
samitkapoor <samitkapoor77@gmail.com>
2025-02-27 16:46:19 +0530

    fixed build issues

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Command, Component, File, Frame, House, Spade, Square, Text } from 'lucide-react';
+import { Command, Component, File, Frame, House, Spade, Text } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
