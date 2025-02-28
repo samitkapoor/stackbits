88f0e6c163c6db5d812c914247854ec375039f9f
samitkapoor <samitkapoor77@gmail.com>
2025-03-01 01:08:10 +0530

    Add LandingFooter and adjust page layout spacing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,17 @@
 import Demo from '@/components/demo';
 import HeroSection from '@/components/hero-section';
+import LandingFooter from '@/components/landing-footer';
 import TechStack from '@/components/tech-stack';
 
 export default function Home() {
   return (
-    <div className="h-full w-full overflow-x-hidden overflow-y-auto flex flex-col bg-black">
+    <div className="h-full w-full overflow-x-hidden overflow-y-auto flex flex-col gap-44 bg-black">
       <div className="mx-auto px-4 sm:px-6 lg:px-8 w-full">
         <HeroSection />
         <TechStack />
         <Demo />
-        <div className="w-full py-8 sm:py-12 md:py-16 lg:py-24"></div>
       </div>
+      <LandingFooter />
     </div>
   );
 }
