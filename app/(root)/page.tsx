8e1f9f7358f53d1e3e321a1bb0ee9d365750670c
samitkapoor <samitkapoor77@gmail.com>
2025-02-28 20:56:47 +0530

    Add TechStack component and remove unused tech stack images

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import FeaturesSection from '@/components/features-section';
 import HeroSection from '@/components/hero-section';
+import TechStack from '@/components/tech-stack';
 
 export default function Home() {
   return (
     <div className="h-full w-screen overflow-x-hidden overflow-y-scroll flex flex-col bg-black">
       <HeroSection />
+      <TechStack />
       {/* <FeaturesSection /> */}
       <div className="w-full h-96"></div>
     </div>
---
+++ components/tech-stack.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const TechStack = () => {
+  const techStack = [
+    {
+      image: '/javascript.svg',
+      name: 'Javascript'
+    },
+    {
+      image: '/typescript.svg',
+      name: 'Typescript'
+    },
+    {
+      image: '/react.svg',
+      name: 'React'
+    },
+    {
+      image: '/nextjs.svg',
+      name: 'NextJS'
+    },
+    {
+      image: '/nodejs.svg',
+      name: 'NodeJS'
+    },
+    {
+      image: '/express.svg',
+      name: 'Express'
+    },
+    {
+      image: '/css.svg',
+      name: 'CSS'
+    },
+    {
+      image: '/tailwindcss.svg',
+      name: 'TailwindCSS'
+    },
+    {
+      image: '/framermotion.png',
+      name: 'Framer Motion'
+    },
+    {
+      image: '/threejs.svg',
+      name: 'ThreeJS'
+    }
+  ];
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center mt-20">
+      <p className="text-white/60 text-2xl font-semibold mt-10">Built For The Modern Stack.</p>
+      <div className="max-w-[1100px] w-full flex flex-wrap items-center justify-center gap-5 mt-10">
+        {techStack.map((tech) => (
+          <motion.div
+            key={tech.name}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0
+            }}
+            transition={{
+              duration: 0.5,
+              delay: 0.1
+            }}
+            whileHover={{
+              rotateZ: 2,
+              transition: {
+                duration: 0.1,
+                delay: 0
+              }
+            }}
+            className="flex items-center justify-center bg-neutral-600/30 px-5 py-5 rounded-xl border-2 border-neutral-600 gap-5"
+          >
+            <Image src={tech.image} alt={tech.name} width={48} height={48} />
+            <p className="text-white text-2xl">{tech.name}</p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
