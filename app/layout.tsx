054eca4ac7391bb29e942975354c04d41a53e064
samitkapoor <samitkapoor77@gmail.com>
2025-03-21 19:59:44 +0530

    added refix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,12 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <script
+          src="https://script.refix.ai/script.min.js"
+          type="text/javascript"
+          data-refix-token="21e99321-89fd-4286-9d23-1d9c70fabbd8"
+          defer
+        ></script>
 
         <SchemaOrgWrapper />
       </head>
