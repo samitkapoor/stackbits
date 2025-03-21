264e4a9c6028637b806a66105acea318adaa92ef
samitkapoor <samitkapoor77@gmail.com>
2025-03-21 13:11:44 +0530

    Add @microsoft/clarity dependency and initialize in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next';
+import Clarity from '@microsoft/clarity';
 import './globals.css';
 import ContactMe from '@/components/contact-me';
 import TopBar from '@/components/top-bar';
 import SchemaOrgWrapper from '@/components/schema-org-wrapper';
+
+Clarity.init('qrkwcfhjk3');
 
 export const metadata: Metadata = {
   title: 'StackBits - React UI & Full-Stack Code Library',
@@ -67,6 +70,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+
         <SchemaOrgWrapper />
       </head>
       <body
