bae32bb040190ac9edd0ebd7d9cd673917d8d694
samitkapoor <samitkapoor77@gmail.com>
2025-05-26 19:30:34 +0530

    enhance TopBar component with a gradient background for improved visual appeal

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -21,41 +21,39 @@ const TopBar = () => {
   const pathname = usePathname();
 
   return (
-    <>
-      <div className="flex items-center justify-between w-full px-4 sm:px-6 md:px-8 h-[40px] sm:h-[50px] md:h-[70px] rounded-md fixed z-[200]">
-        <div className="overflow-hidden h-full">
-          <Link
-            href="/"
-            className="flex items-center gap-1 sm:gap-2 h-full justify-center ml-1 sm:ml-2 text-xs sm:text-sm md:text-base"
-          >
-            <CodeXml className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6" />
-            Stackbits
-          </Link>
-        </div>
-
-        {/* Desktop navigation */}
-        <div className="flex items-center text-white gap-4">
-          {links.map((link) => {
-            return (
-              <Link key={link.name} href={link.href}>
-                <GlassButton
-                  wrapperClassName={`px-1 sm:px-2 md:px-3 py-1 sm:py-2 !rounded-md`}
-                  className={`flex items-center gap-1 text-xs sm:text-sm transition-all`}
-                >
-                  {link.icon}
-                  <p className="">{link.name}</p>
-                </GlassButton>
-              </Link>
-            );
-          })}
-          <Link href="/prompt">
-            <AnimatedGradientButton className="!px-4 !py-1 md:!py-2 text-sm">
-              Request
-            </AnimatedGradientButton>
-          </Link>
-        </div>
+    <div className="flex items-center justify-between w-full px-4 sm:px-6 md:px-8 h-[40px] sm:h-[50px] md:h-[70px] rounded-md fixed z-[200] bg-gradient-to-b from-black to-transparent">
+      <div className="overflow-hidden h-full">
+        <Link
+          href="/"
+          className="flex items-center gap-1 sm:gap-2 h-full justify-center ml-1 sm:ml-2 text-xs sm:text-sm md:text-base"
+        >
+          <CodeXml className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6" />
+          Stackbits
+        </Link>
       </div>
-    </>
+
+      {/* Desktop navigation */}
+      <div className="flex items-center text-white gap-4">
+        {links.map((link) => {
+          return (
+            <Link key={link.name} href={link.href}>
+              <GlassButton
+                wrapperClassName={`px-1 sm:px-2 md:px-3 py-1 sm:py-2 !rounded-md`}
+                className={`flex items-center gap-1 text-xs sm:text-sm transition-all`}
+              >
+                {link.icon}
+                <p className="">{link.name}</p>
+              </GlassButton>
+            </Link>
+          );
+        })}
+        <Link href="/prompt">
+          <AnimatedGradientButton className="!px-4 !py-1 md:!py-2 text-sm">
+            Request
+          </AnimatedGradientButton>
+        </Link>
+      </div>
+    </div>
   );
 };
 
