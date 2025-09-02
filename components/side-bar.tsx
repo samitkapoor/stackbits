74499fa5be131ebc2c8af538a882ec03b21e87e7
samitkapoor <samitkapoor77@gmail.com>
2025-09-02 20:08:44 +0530

    refactor layout and sidebar components for improved styling and functionality; replace MovingBorderButton with standard buttons, adjust sidebar positioning, and enhance TopBar layout

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -7,6 +7,7 @@ import { categories, getSideBarTabs } from '@/data/main';
 import RainbowText from './ui/rainbow-text';
 import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 const SideBar = ({ isOpen }: { isOpen: boolean }) => {
   const tabs = getSideBarTabs();
@@ -20,28 +21,29 @@ const SideBar = ({ isOpen }: { isOpen: boolean }) => {
       className={
         `${
           isOpen
-            ? `absolute lg:static left-[20px] md:left-[50px] pb-10`
+            ? `absolute lg:static left-[0px] w-full z-[900]`
             : `absolute -left-[350px] lg:static`
-        } ` + `backdrop-blur-md h-full flex flex-col w-[350px] z-40 `
+        } ` + `backdrop-blur-md h-full flex flex-col w-[350px] z-40 px-3`
       }
     >
-      <div className="h-full overflow-y-auto scrollbar-hide flex flex-col gap-2 w-full z-40 pt-20">
+      <div className="h-full overflow-y-auto scrollbar-hide flex flex-col gap-8 w-full z-40 pt-10 md:pt-20">
         {tabs.map((group) => {
           return (
             <div key={group.title} className="flex gap-2 flex-col items-start">
               {categories.includes(group.title.toLowerCase()) ? (
                 <a
                   href={`/docs/${group.title.toLowerCase()}`}
-                  className={
-                    (pathname?.split('docs/')[1] === group.title.toLowerCase()
+                  className={cn(
+                    'text-xs text-white',
+                    pathname?.split('docs/')[1] === group.title.toLowerCase()
                       ? 'text-yellow-400'
-                      : 'hover:text-yellow-400 transition-all') + ' font-medium text-sm'
-                  }
+                      : 'hover:text-yellow-400 transition-all'
+                  )}
                 >
                   {group.title}
                 </a>
               ) : (
-                <p className="font-medium text-sm">{group.title}</p>
+                <p className="text-xs text-white">{group.title}</p>
               )}
 
               <div className="flex flex-col w-full">
@@ -63,16 +65,17 @@ const SideBar = ({ isOpen }: { isOpen: boolean }) => {
                             layout
                             layoutId="side-bar-highlight"
                             transition={{ duration: 0.2 }}
-                            className="border-l-[2px] border-yellow-500 bg-gradient-to-r from-yellow-500/10 rounded-md z-0 absolute top-[-5px] w-full h-[calc(100%+10px)]"
+                            className="w-px bg-yellow-500 z-0 absolute top-[0px] h-full"
                           />
                         )}
                         <div className="border-l-[2px] border-white/10 z-0 absolute inset-0" />
                         <p
-                          className={
-                            (isActive
-                              ? 'text-yellow-400'
-                              : 'hover:text-yellow-400 transition-all') + ' z-20 relative pl-4'
-                          }
+                          className={cn(
+                            'z-20 relative pl-4 text-sm',
+                            pathname === child.href
+                              ? 'text-white font-medium'
+                              : 'text-white/70 hover:text-white transition-all'
+                          )}
                         >
                           {child.name}
                         </p>
--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -20,7 +20,7 @@ const TopBar = () => {
 
   return (
     <div className="flex flex-col w-full fixed h-[40px] sm:h-[50px] md:h-[70px] z-[200]">
-      <div className="fixed top-0 w-full flex items-start justify-center h-[50px] z-10">
+      <div className="fixed top-0 w-full flex items-start justify-center h-[30px] z-0">
         <motion.div
           animate={{
             background: [
@@ -40,16 +40,25 @@ const TopBar = () => {
           className="w-full h-full absolute bottom-8 pointer-events-none left-0 z-0 blur-3xl"
         />
       </div>
+      <div
+        style={{
+          backdropFilter: 'blur(10px)',
+          WebkitBackdropFilter: 'blur(10px)',
+          maskImage: 'linear-gradient(to top, transparent, black)',
+          WebkitMaskImage: 'linear-gradient(to top, transparent, black)'
+        }}
+        className="fixed top-0 w-full flex items-start justify-center h-[40px] sm:h-[50px] md:h-[70px] z-0"
+      ></div>
 
-      <div className="z-10 w-full flex gap-2 items-center justify-center border-b border-black text-xs bg-black">
+      <div className="z-10 w-full gap-2 items-center justify-center border-b border-black text-xs bg-black hidden md:flex">
         Stackbits is going through a refresh! Some features may not be available. Follow @stackbitss
         on twitter for updates.
       </div>
-      <div className="flex items-center justify-between w-full px-4 sm:px-6 md:px-8 h-full bg-gradient-to-b from-black to-transparent">
+      <div className="flex items-center justify-between w-full px-3 pt-5 md:pt-0 h-full bg-gradient-to-b from-black to-transparent z-10">
         <div className="overflow-hidden h-full">
           <Link
             href="/"
-            className="flex items-center gap-1 sm:gap-2 h-full justify-center ml-1 sm:ml-2 text-sm md:text-base"
+            className="flex items-center gap-2 h-full justify-center ml-1 sm:ml-2 text-base"
           >
             <CodeXml className="w-5 h-5 md:w-6 md:h-6" />
             Stackbits
@@ -57,23 +66,21 @@ const TopBar = () => {
         </div>
 
         {/* Desktop navigation */}
-        <div className="flex items-center text-white gap-2 ">
+        <div className="flex items-center text-white gap-2 pr-11 lg:pr-0 z-10">
           {links.map((link) => {
             return (
               <Link key={link.name} href={link.href}>
-                <MovingBorderButton
-                  className={`flex items-center gap-1 text-xs sm:text-sm transition-all`}
-                >
+                <button className="rounded-md bg-black h-9 w-min px-2 flex items-center justify-center gap-1 text-sm">
                   {link.icon}
                   <p className="text-xs sm:text-sm">{link.name}</p>
-                </MovingBorderButton>
+                </button>
               </Link>
             );
           })}
-          <Link href="https://github.com/samitkapoor/stackbits">
-            <ShineButton className="!px-2 md:!px-3 !py-2 !rounded-md text-xs sm:text-sm">
+          <Link href="https://github.com/samitkapoor/stackbits" target="_blank">
+            <button className="rounded-md bg-black h-9 w-min px-2 flex items-center justify-center text-sm">
               Github
-            </ShineButton>
+            </button>
           </Link>
         </div>
       </div>
