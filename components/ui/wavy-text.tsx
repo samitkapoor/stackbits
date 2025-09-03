f87d42bbfa6ed11d2be064e08b708772ec47359e
samitkapoor <samitkapoor77@gmail.com>
2025-09-03 11:56:26 +0530

    refactor WavyText component: improve className handling with utility function, enhance animation with smoother transitions, and update installation documentation to include new code snippets

--- components/ui/wavy-text.tsx
+++ components/ui/wavy-text.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils';
 import { motion } from 'framer-motion';
 import { FC } from 'react';
 
@@ -8,17 +9,17 @@ type WavyTextProps = {
 
 const WavyText: FC<WavyTextProps> = ({ text, className }) => {
   return (
-    <div className={`flex flex-wrap overflow-visible p-4 ${className}`}>
+    <div className={cn('flex flex-wrap overflow-visible p-4', className)}>
       {text.split('').map((char, index) => (
         <motion.span
           key={index}
           initial={{ y: '100%' }}
-          animate={{ y: [0, -15] }}
+          animate={{ y: [0, -15, 0] }}
           transition={{
-            type: 'spring',
-            stiffness: 100,
-            damping: 20,
+            duration: 0.5,
+            ease: 'easeInOut',
             delay: index * 0.1,
+            repeatDelay: (text.split('').length / 2) * 0.1,
             repeat: Infinity,
             repeatType: 'reverse'
           }}
--- data/text/WavyText.tsx
+++ data/text/WavyText.tsx
@@ -1,3 +1,4 @@
+import { cnCode, installDependenciesCode } from '@/constants/code';
 import { Document } from '../main';
 import WavyText from '@/components/ui/wavy-text';
 
@@ -26,11 +27,14 @@ export const wavyText: Document = {
         sectionType: 'preview',
         code: <WavyText text="Wavy Text" className="text-4xl" />
       },
+      installDependenciesCode({ framerMotion: true }),
+      cnCode,
       {
         heading: 'Wavy Text',
         sectionType: 'component',
         description: 'Create a file wavy-text.tsx in your components folder and paste this code',
-        code: `import { motion } from 'framer-motion';
+        code: `import { cn } from '@/lib/utils';
+import { motion } from 'framer-motion';
 import { FC } from 'react';
 
 type WavyTextProps = {
@@ -40,17 +44,17 @@ type WavyTextProps = {
 
 const WavyText: FC<WavyTextProps> = ({ text, className }) => {
   return (
-    <div className={\`flex flex-wrap overflow-visible p-4 \${className}\`}>
+    <div className={cn('flex flex-wrap overflow-visible p-4', className)}>
       {text.split('').map((char, index) => (
         <motion.span
           key={index}
           initial={{ y: '100%' }}
-          animate={{ y: [0, -15] }}
+          animate={{ y: [0, -15, 0] }}
           transition={{
-            type: 'spring',
-            stiffness: 100,
-            damping: 20,
+            duration: 0.5,
+            ease: 'easeInOut',
             delay: index * 0.1,
+            repeatDelay: (text.split('').length / 2) * 0.1,
             repeat: Infinity,
             repeatType: 'reverse'
           }}
