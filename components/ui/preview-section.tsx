7659962ea6d5933cbc4d8bd52789fd93d2a52719
samitkapoor <samitkapoor77@gmail.com>
2025-09-13 19:55:02 +0530

    refactor DocumentContentBox component: replace inline preview section with PreviewSection component for cleaner structure and improved readability

---
+++ components/ui/preview-section.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import React, { ReactNode, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Maximize, Minimize } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+const PreviewSection = ({ code }: { code: ReactNode }) => {
+  const [fullScreen, setFullScreen] = useState(false);
+
+  const buttons = {
+    false: {
+      icon: <Maximize />,
+      onClick: () => {
+        console.log('maximize');
+        setFullScreen(true);
+      }
+    },
+    true: {
+      icon: <Minimize />,
+      onClick: () => {
+        console.log('minimize');
+        setFullScreen(false);
+      }
+    }
+  };
+
+  return (
+    <motion.div
+      // layout
+      // transition={{
+      //   duration: 0.8,
+      //   ease: 'easeInOut'
+      // }}
+      className={cn(
+        'w-full h-[calc(100vh-50px)] p-9 lg:p-4 z-50',
+        fullScreen && 'fixed bottom-0.5 right-0 w-screen h-[calc(100vh-50px)]'
+      )}
+    >
+      <div className="h-full w-full border border-white/5 bg-[#111111] rounded-xl flex items-center justify-center min-h-[500px] relative overflow-x-hidden">
+        <motion.button
+          onClick={() => buttons[`${fullScreen}`].onClick()}
+          className="absolute top-4 right-4 z-50 bg-black/10 p-2"
+        >
+          <AnimatePresence mode="popLayout">
+            <motion.div
+              key={`${fullScreen}-icon`}
+              initial={{ opacity: 0, scale: 0.3 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.3 }}
+              transition={{
+                duration: 0.2,
+                ease: 'easeInOut'
+              }}
+            >
+              {buttons[`${fullScreen}`].icon}
+            </motion.div>
+          </AnimatePresence>
+        </motion.button>
+        {code}
+      </div>
+    </motion.div>
+  );
+};
+
+export default PreviewSection;
